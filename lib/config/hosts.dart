03861c58e83f82983ac7c4deb6ba8d1ede5b380a
Agung Nurcahyo Rosian Dana <agungnurcahyo16@gmail.com>
2023-09-09 18:19:44 +0700

    update gitignore

--- lib/config/hosts.dart
+++
@@ -1,6 +0,0 @@
-// const uploadfile = 'http://192.168.100.100:8080';
-// const host = 'http://192.168.100.100:8080';
-// const wshost = 'ws://192.168.100.100:8080';
-const uploadfile = 'https://fishventure.site';
-const host = 'https://fishventure.site';
-const wshost = 'wss://fishventure.site';
